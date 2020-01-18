9ffedf03dcaf7058469ac45385b26380268993c6
Justin Branch <justinms@outlook.com>
2020-01-18 04:03:21 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,89 @@
+class TicTacToe
+  
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end 
+  
+   def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(user_input)
+    user_input = user_input.to_i - 1
+  end
+  
+  def move(index, token="X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    @board[index] == " " ? false : true
+  end 
+  
+  def valid_move?(index)
+    if @board[index] && !position_taken?(index)
+      return true
+    else
+      return false
+    end 
+  end 
+  
+  def turn_count
+    @board.count{|index| index != " "} 
+  end 
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def turn
+    puts "Enter board position 1-9: "
+    input = gets.strip
+    position = input_to_index(input)
+    
+    if valid_move?(position)
+      move(position, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def won? 
+    WIN_COMBINATIONS.each do |array|
+      x = 0
+      o = 0 
+      array.each do |i|
+        if @board[i] == "X"
+          x += 1 
+        elsif @board[i] == "O"
+          o += 1
+        end 
+        
+        if x == 3 || o == 3
+          return array
+        end
+      end
+    end
+    false
+  end
+  
+  def full?
+    @board.all? {|position| position == "X" || "O"}
+  end
+end 
